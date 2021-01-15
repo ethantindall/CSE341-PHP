a4fae26209ed1f5ac2f8ba7db748c47396e327ea
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-01-15 16:56:17 -0700

    update

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="main.css">
+    <style><?php include "main.css" ?></style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="main.js"></script>
     <title>CSE341 Home Page</title>
