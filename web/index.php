2eeaf092d8802838854b6a5a59fbceaad562aa69
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-01-15 16:51:19 -0700

    update

--- web/index.php
+++ web/index.php
@@ -1,20 +1,19 @@
 <?php
 
-
-// Create or access a Session
 session_start();
 
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
     if ($action == NULL){
         $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
  }
+
  switch ($action){
     case 'assignments': 
-        include 'web/homepage/assignments.php';
-        exit;
+        include 'homepage/assignments.php';
+        break;
     default:
-        include 'web/homepage/homepage.php';
-        exit;
+        include 'homepage/homepage.php';
+        break;
     }
     
 
