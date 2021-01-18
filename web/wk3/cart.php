c64c5d61fe68525e74cbf1e1897052e25a36dbc6
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-01-18 08:52:40 -0700

    update

---
+++ web/wk3/cart.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="stylesheet" href="main.css">
+    <title>CSE341 Wk 3 Cart</title>
+
+</head>
+<body>
+    <header><h1>Shopping Cart</h1></header>
+
+    <main>
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/cat" alt="Cat toys">
+            <h2>Cat Toy</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/dog" alt="Dog toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/dolphin" alt="Dolphin toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/panda" alt="Panda toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/giraffe" alt="Giraffe toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/zebra" alt="Zebra toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/goldfish" alt="Goldfish toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/lion" alt="Lion toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+        <div class="card">
+            <img src="https://loremflickr.com/150/100/octopus" alt="Octopus toy">
+            <h2>Cat Toys</h2>
+        </div>
+
+
+    </main>
+
+    <footer></footer>
+</body>
+</html>
