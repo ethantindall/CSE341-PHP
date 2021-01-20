b3a2880dd0c8371c6873be03e13067191b9c721d
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-01-20 10:48:03 -0700

    wk3

--- web/wk3/cart.php
+++ web/wk3/cart.php
@@ -3,62 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="stylesheet" href="main.css">
+    <style><?php include "main.css" ?></style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>CSE341 Wk 3 Cart</title>
 
 </head>
 <body>
-    <header><h1>Shopping Cart</h1></header>
+    <header><h1>Your Cart</h1>    
+    <a href="/CSE341-PHP/web/wk3/"><button class="viewStore">View Store</button></a>
+</header>
 
-    <main>
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/cat" alt="Cat toys">
-            <h2>Cat Toy</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/dog" alt="Dog toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/dolphin" alt="Dolphin toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/panda" alt="Panda toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/giraffe" alt="Giraffe toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/zebra" alt="Zebra toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/goldfish" alt="Goldfish toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/lion" alt="Lion toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-        <div class="card">
-            <img src="https://loremflickr.com/150/100/octopus" alt="Octopus toy">
-            <h2>Cat Toys</h2>
-        </div>
-
-
+    <main class="shoppingCart">
+        <?php if (isset($_SESSION['cartItem'])) { 
+            echo $_SESSION['cartItem'];
+         } ?>
+    
     </main>
 
-    <footer></footer>
+    <?php if (isset($_SESSION['cartItem']) && $_SESSION['cartItem'] != 'You have no items in your cart.') { echo '<footer><a href="/CSE341-PHP/web/wk3/?action=checkoutPage"><button>Proceed To Checkout</button></a></footer>';} ?>
 </body>
 </html>
