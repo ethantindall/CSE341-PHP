2644468f1133ce74d9f05316b7177a910f598e9b
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-02 14:48:15 -0700

    update

--- web/wk5-team/results.php
+++ web/wk5-team/results.php
@@ -1,6 +1,29 @@
 <?php 
+function connect_to_db() {
+    try {
+        $dbUrl = getenv('DATABASE_URL');
 
-require 'connect.php';
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+
+    } catch (PDOException $ex) {
+        
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+}
+
 $db = connect_to_db();
 
 ?><!DOCTYPE html>
