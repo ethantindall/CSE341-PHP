591361683923c4fe017ed535d1c75ca564f7a731
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-02 10:36:42 -0700

    update

---
+++ web/wk5-team/results.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Wk 5 Team</title>
+
+</head>
+<body>
+    <ul>
+        <li>Book: <?php echo $_SESSION['book'] ?></li>
+        <li>Chapter: <?php echo $_SESSION['chapter'] ?></li>
+        <li>Verse: <?php echo $_SESSION['verse'] ?></li>
+        <li>Content: <?php echo $_SESSION['content'] ?></li>
+    </ul>
+</body>
+</html>
