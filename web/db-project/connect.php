7b6af2072fb52a4aaf05c12ac1cd4867e6328d1a
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-03 14:20:23 -0700

    update

--- web/db-project/connect.php
+++ web/db-project/connect.php
@@ -43,7 +43,7 @@ function searchresults($company, $searchby, $textinput) {
 
     if ($company == 'strataInventory') {
         $dbquery = 'SELECT item_id, item_sticker_id, item_name, item_quantity, item_checked_out FROM ' 
-            . $company . ' WHERE item_name  LIKE "%' . $searchParameters . '%"';
+            . $company . " WHERE item_name  LIKE '%" . $textinput . "%'";
 
         echo ucfirst($_POST['company'] . ' Inventory');
         foreach ($db->query($dbquery) as $row) {
@@ -59,7 +59,7 @@ function searchresults($company, $searchby, $textinput) {
     }
     else if ($company == 'spectraInventory') {
         $dbquery = 'SELECT item_id, item_name, item_quantity, item_owner, item_checked_out FROM ' 
-        . $company . " WHERE item_name  LIKE '%" . $searchParameters . "%'";
+        . $company . " WHERE item_name  LIKE '%" . $textinput . "%'";
 
         echo ucfirst($_POST['company'] . ' Inventory');
         foreach ($db->query($dbquery) as $row) {
