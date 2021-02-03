9dcf5bccaab1b17340505e2df94699bf8d204421
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-02 19:35:54 -0700

    update

--- web/wk5-team/results.php
+++ web/wk5-team/results.php
@@ -18,7 +18,7 @@ require 'connect.php';
     echo 'hi';
     $db = connect_to_db();
     echo $_POST['book_name'];
-    foreach ($db->query("SELECT book, chapter, verse, content FROM scriptures WHERE book LIKE %{$_POST['book_name']}%") as $row) {
+    foreach ($db->query("SELECT book, chapter, verse, content FROM scriptures WHERE book LIKE '%{$_POST["book_name"]}%'") as $row) {
         echo 'Scripture: ' . $row['book'] .' ' . $row['chapter'] . ':' . $row['verse'] . '<br/>';
     }
 
