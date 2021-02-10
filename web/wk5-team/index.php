cb8c9e2e063bd8ed71d4788cbf1bc8fcbc5dbd99
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-10 13:43:57 -0700

    update

--- web/wk5-team/index.php
+++ web/wk5-team/index.php
@@ -24,13 +24,13 @@ switch ($action){
         break;
     case 'addToDatabase':
 
-        $_POST['book'] = filter_input(INPUT_GET, 'book', FILTER_SANITIZE_STRING);
-        $_POST['chapter'] = filter_input(INPUT_GET, 'chapter', FILTER_VALIDATE_INT);
-        $_POST['verse'] = filter_input(INPUT_GET, 'verse', FILTER_VALIDATE_INT);
-        $_POST['content'] = filter_input(INPUT_GET, 'content', FILTER_SANITIZE_STRING);
+        $_SESSION['book'] = filter_input(INPUT_GET, 'book', FILTER_SANITIZE_STRING);
+        $_SESSION['chapter'] = filter_input(INPUT_GET, 'chapter', FILTER_VALIDATE_INT);
+        $_SESSION['verse'] = filter_input(INPUT_GET, 'verse', FILTER_VALIDATE_INT);
+        $_SESSION['content'] = filter_input(INPUT_GET, 'content', FILTER_SANITIZE_STRING);
 
-        echo $_POST['book'];
-
+        echo $_SESSION['book'];
+        
         include 'addresults.php';
         break;
     default:
