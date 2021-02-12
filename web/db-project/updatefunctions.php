c1659ea75260f5d2736ae138ddb04d307d5bf569
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-12 08:52:25 -0700

    update

--- web/db-project/updatefunctions.php
+++ web/db-project/updatefunctions.php
@@ -2,9 +2,9 @@
 
 function addToDatabase($company, $stickerId, $name, $quantity, $purchaseDate, $checkedOut, $checkedOutDate, $checkedOutBy, $description) {
     if ($company == 'strata') {
-        $query = 'INSERT INTO :company (item_sticker_id, item_name, item_description, item_quantity
+        $query = 'INSERT INTO :company (item_sticker_id, item_name, item_description, item_quantity, item_storage_location,
         item_purchase_date, item_checked_out, item_checked_out_date, item_checked_out_by) 
-        VALUES (:sticker, :name, :desc, :quantity, :purchase, :check, :checkdate, :checkby)';
+        VALUES (:sticker, :iname, :idesc, :quantity, 0, :purchase, :icheck, :checkdate, :checkby)';
     }
     if ($company == 'spectra') {
         $query = 'INSERT INTO :company (item_sticker_id, item_name, item_description, item_quantity
@@ -19,13 +19,13 @@ function addToDatabase($company, $stickerId, $name, $quantity, $purchaseDate, $c
 
     $stmt->bindValue(':company', $company, PDO::PARAM_INT);
     $stmt->bindValue(':sticker', $stickerId, PDO::PARAM_INT);
-    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':iname', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':idesc', $description, PDO::PARAM_INT);
     $stmt->bindValue(':quantity', $quantity, PDO::PARAM_INT);
     $stmt->bindValue(':purchase', $purchaseDate, PDO::PARAM_INT);
-    $stmt->bindValue(':check', $checkedOut, PDO::PARAM_INT);
+    $stmt->bindValue(':icheck', $checkedOut, PDO::PARAM_INT);
     $stmt->bindValue(':checkdate', $checkedOutDate, PDO::PARAM_INT);
     $stmt->bindValue(':checkby', $checkedOutBy, PDO::PARAM_INT);
-    $stmt->bindValue(':desc', $description, PDO::PARAM_INT);
     
     $stmt->execute();
 
