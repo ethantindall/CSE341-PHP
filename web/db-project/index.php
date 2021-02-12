a6833c441a8bdb81447723efc5ab772b58870489
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-11 20:37:21 -0700

    update

--- web/db-project/index.php
+++ web/db-project/index.php
@@ -14,16 +14,8 @@ $_SESSION['message'] = '';
         include 'add.php';
         break;
     case 'addToDatabase':
+        $name = $_POST['name'];
 
-        $company = filter_input(INPUT_POST, 'company', FILTER_SANITIZE_STRING);
-        $stickerId = filter_input(INPUT_POST, 'stickerId', FILTER_SANITIZE_NUMBER_INT);
-        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-        $quantity = filter_input(INPUT_POST, 'quantity', FILTER_SANITIZE_NUMBER_INT);
-        $purchaseDate = filter_input(INPUT_POST, 'purchaseDate', FILTER_SANITIZE_STRING);
-        $checkedOut = filter_input(INPUT_POST, 'checkedOut', FILTER_SANITIZE_STRING);
-        $checkedOutDate = filter_input(INPUT_POST, 'checkedOutDate', FILTER_SANITIZE_STRING);
-        $checkedOutBy = filter_input(INPUT_POST, 'checkedOutBy', FILTER_SANITIZE_STRING);
-        $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 
         echo $name;
         $_SESSION['message'] = 'name: ' .$name;
