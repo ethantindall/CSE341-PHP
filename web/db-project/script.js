fb48d2c9de566c1b77ba99f7126130e355222f88
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-12 16:24:56 -0700

    update

--- web/db-project/script.js
+++ web/db-project/script.js
@@ -1,13 +1,3 @@
 function when(x) {
-    if (x == 0) {
-        document.getElementById('add-sticker').disabled= false;
-        console.log(x + "yeah");
-    }
-    else if (x == 1) {
-        document.getElementById('add-sticker').disabled= true;
-        console.log(x + "ok");
-    }
-    else {
-        console.log("uh-oh");
-    }
+    console.log('hi');
 }
