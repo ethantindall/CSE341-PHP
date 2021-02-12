bfc1ce326f2b17e309e62a44e5ae2e16ac4da112
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-02-12 08:19:57 -0700

    update

--- web/db-project/updatefunctions.php
+++ web/db-project/updatefunctions.php
@@ -31,4 +31,30 @@ function addToDatabase($company, $stickerId, $name, $quantity, $purchaseDate, $c
 
 }
 
+
+
+function connect_to_db() {
+    try {
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+
+    } catch (PDOException $e) {
+        
+        echo 'Error!: ' . $e->getMessage();
+        die();
+    }
+}
 ?>
